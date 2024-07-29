431cba405c6989eeacc0877e3321c2fcf1c6863a
parth136 <parth@sanskartechnolab.com>
2024-07-29 18:25:19 +0530

    day 1 commit - created aeroplane app and completed day 1 tasks in tat app.

---
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024, Parth and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document 
+
+class FlightPassenger(Document):
+	# @property
+	# def full_name(self):
+	# 	return f"{self.first_name} {self.last_name}"
+    pass
+	
