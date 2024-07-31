b1273c9287139c0cd8dcdd533a90d45d9e9bf626
parth136 <parth@sanskartechnolab.com>
2024-07-31 18:33:51 +0530

    Day-2 Tasks Completed (which includes web forms, notification and controllers for airplane ticket doctype and created new doctype airplane flight and write an onsubmit event in js file and refactor the flow in airplane ticket.)

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,48 @@
 // Copyright (c) 2024, Parth and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+    
+    refresh(frm) {
+        
+	},
+    
+    
+    
+    
+    after_save(frm) {
+        
+        console.log("----------------",frm.doc.status);
+        let data = frm.doc
+        
+        frappe.call({
+            doc:frm.doc,
+            method:"flight_addons_function",
+            args:{
+              data:data
+            },
+            freeze:true,
+            freeze_message:"This is freeze message...",
+            callback(e){
+                //   console.log(e);
+                frappe.msgprint(e.message)
+            }
+            
+        })
+	},
+    
+    
+    
+    // on_submit
+    before_submit(frm){
+        
+        if(frm.doc.status !== "Boarded"){
+            frappe.throw("You Cannot submit the document")
+            // return false
+        }
+    },
+    
+    
+    
 
-// 	},
-// });
+});
